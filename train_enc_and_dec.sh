1caced7209bf150af5118af41f867012de280b6f
zhanghuimeng <zhanghuimeng1997@gmail.com>
2019-11-29 19:42:10 +0800

    The adapter proved not successful. Ready to do some validation

--- train_enc_and_dec.sh
+++ train_enc_and_dec.sh
@@ -1,9 +1,9 @@
 export PYTHONPATH=/home1/private/zhm/201910_cotrain/dep/THUMT:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES=4,5
+export CUDA_VISIBLE_DEVICES=0,1
 # 检查拼起来的enc-dec的效果
 python THUMT/thumt/bin/train_enc_and_dec.py \
     --input data/corpus.tc.32k.en.shuf data/corpus.tc.32k.de.shuf \
-    --output en-de-concat-model/prac-small \
+    --output en-de-concat-model/small \
     --vocabulary data/vocab.32k.en.txt data/vocab.32k.de.txt \
     --enc_ckpt ckpt/en-de-trans/model.ckpt-161001 \
     --dec_ckpt ckpt/de-frozen-ae/model.ckpt-186001 \
